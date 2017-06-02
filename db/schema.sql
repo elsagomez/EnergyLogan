0ff3eeeeebd1e8f92f019dae5df5cf5755a7c23f
lasperilla <l.asperilla@gmail.com>
2017-06-01 20:44:14 -0400

    worked on routes, fixed model names on models, testing out newproject post

--- db/schema.sql
+++ db/schema.sql
@@ -11,20 +11,24 @@ CREATE TABLE projects(
   contact_name VARCHAR(255) NOT NULL,
   contact_number VARCHAR(255) NOT NULL,
   account_number INT NOT NULL,
-  number_of_floors INT NOT NULL,
+  num_floors INT NOT NULL,
   other VARCHAR(255) NOT NULL,
   scheduled_date DATE,
   comments VARCHAR(500),
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (project_id)
 );
 
-CREATE TABLE survey(
+CREATE TABLE surveys(
 	survey_id INT NOT NULL AUTO_INCREMENT,
   project_id INT NOT NULL,
   floor_number INT,
   room VARCHAR(255) NOT NULL,
   fixture_id VARCHAR(255) NOT NULL,
   quantity INT NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (survey_id)
 );
 
@@ -37,6 +41,8 @@ CREATE TABLE fixtures(
   num_lamps INT,
   watts_per_lamp INT,
   watts_per_fixture INT,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (fixture_id)
 );
 
