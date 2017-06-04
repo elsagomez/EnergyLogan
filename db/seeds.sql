bb0bae6b4e64c70d94cc5aa7ea46bddd993a6b26
elsagomez <elsa.ghdz@gmail.com>
2017-06-03 21:35:41 -0400

    fix merge conflict

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,10 +3,10 @@ USE energylogan_db;
 INSERT INTO users
 	(email, password, user_type)
 VALUES
-	("elsa.ghdz@gmail.com", "elsapass", "office"),
-	("lawrence@gmail.com", "lawpass", "technical"),
-	("april@gmail.com", "aprilpass", "office"),
-	("alex@gmail.com", "alexpass", "technical");
+	("elsa.ghdz@gmail.com", "$2a$10$QkZAyLejqRy7KOa5gq.x4.mYkq/ZQwpGn9/kC1zV0QS.UGhnRIoQW", "office"),
+	("lawrence@gmail.com", "$2a$10$7DE7/5XOYADyBs7qWNP6w.hGvu.kOO9ccFF.FToYTjS.Vq1NqDHty", "technical"),
+	("april@gmail.com", "$10$qXtIh8QkkQxARC1mXdtODeU7kO1/lymPoUhsffr/mMZ/bHL4hcM2S", "office"),
+	("alex@gmail.com", "$10$AWlV.bQtNgkJ9yGsudht1.eMgj0PRbM3Co3ljRRl2L8cg0Kaulkpy", "technical");
 
 
 INSERT INTO projects
@@ -16,43 +16,7 @@ VALUES
   ("101 Easy Street", "Wall Street Inc.", "101 Easy Street, NY", "Jack Moneybags", "2127777777", "201000777", "25", "N/A", "2017-07-11"),
   ("Cruise Plaza", "My Caribbean Cruises", "555 Carribean Way, Miami, FL", "Jack Sparrow", "8351234567", "107000333", "20", "Boiler Room", "2017-06-29"); 
 
-<<<<<<< HEAD
-  INSERT INTO surveys
-  (project_id, floor_number, room, fixture_id, quantity)
-VALUES 
-	(1, 1, "Hallway", 12345, 200),
-	(1, 1, "Room 1", 22222, 4),
-	(1, 2, "Room 1", 22222, 10),
-	(1, 3, "Hallway", 12345, 200),
-	(1, 3, "Bathroom", 23654, 30),
-	(1, 4, "Hallway", 12345, 50),
-	(1, 4, "Room 1", 02555, 10),
-	(1, 4, "Room 2", 12456, 55),
-	(1, 4, "Room 3", 44444, 150)
-	(2, 1, "Lobby", 25566, 300),
-	(2, 2, "Room 1", 22222, 4),
-	(2, 2, "Room 2", 33333, 10),
-	(2, 3, "Hallway", 12345, 200)
-	(3, 1, "Lobby", 12345, 20),
-	(3, 1, "Bathroom", 23422, 4),
-	(3, 2, "Room 1", 22222, 10),
-	(3, 3, "Hallway", 12345, 200),
-	(3, 3, "Bathroom", 23654, 30),
-	(3, 4, "Hallway", 12345, 50),
-	(3, 4, "Room 1", 02555, 10),
-	(3, 4, "Room 2", 12456, 55),
-	(3, 4, "Room 3", 44444, 150);
 
-
-	INSERT INTO prefixtures
-  (preFixID, pre_lampCode, pre_type, pre_desccription, pre_ballast, pre_lampNum, pre_watts, pre_wattsPerFix)
-
-  VALUES 
-	("CF10/2D", "CFD10W", "CFL", "Compact Fluorescent, 2D, (1) 10W lamp","Mag-STD", 1, 10, 16),
-	("ECF5/1", "CFT5W", "EXIT", "EXIT Compact Fluorescent, (1) 5W lamp, 2D, (1) 10W lamp","Mag-STD", 1, 5, 9),
-	("H72/1", "H72", "HAL", "Halogen Incandescent, (1) 72W lamp","HAL-STD", 1, 72, 72);
-	
-=======
 INSERT INTO surveys
   (project_id, floor_number, room, fixture_id, quantity)
 VALUES 
@@ -1511,4 +1475,4 @@ VALUES
 ('GAI-10-LM-A19-E26-8.5W dupe', '', '', 'A19 LED', '', null, 8.5, 8.5),
 ('GAI-06-LED-T5-4-15W-4000K dupe', '', 'LED T5', 'LED T5 SINGLE INTEGRATED TUBE - 4FT - 15W - 4000K DIM', '', 1, 15, 15),
 ('No Replacement', '', '', 'N/A', '', null, 0, 0)
->>>>>>> 52c62a8f3c4953db7e7e4fc1df3c4b1d49428c31
+
