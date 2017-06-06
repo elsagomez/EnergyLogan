781fa628097f2f9380ef266f382ef15dcd276333
lasperilla <l.asperilla@gmail.com>
2017-06-06 15:25:38 -0400

    fixed survey POST

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -147,6 +147,7 @@ module.exports = function(app) {
                                 css: '<link rel="stylesheet" type="text/css" href="../../css/style.css"> <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">',
                                 surveys: [],
                                 floor_number: dbFloors[0].floor_number,
+                                floor_id: dbFloors[0].floor_id,
                                 building: dbFloors[0].Project.project_name,
                                 project_id: dbFloors[0].Project.project_id
                             };
