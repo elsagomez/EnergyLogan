786d30fea87eabf2a5e1022498682e56923fe5db
lasperilla <l.asperilla@gmail.com>
2017-06-06 14:43:36 -0400

    editing nextfloor button

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,53 +1,39 @@
-
-
 $(document).ready(function() {
 
- 
+    var roomInput = $("#room")
+    var fixtureInput = $("#fixture");
+    var qtyInput = $("#num_fixture");
+    var roomForm = $("#roomLine");
 
+    $(roomForm).on("submit", handleLineSubmit);
 
-        var roomInput = $("#room")
-        var fixtureInput = $("#fixture");
-        var qtyInput =$("#num_fixture");
-        var roomForm = $("#roomLine");
-
-        $(roomForm).on("submit", handleLineSubmit);
-
-     function handleLineSubmit(event){
+    function handleLineSubmit(event) {
         event.preventDefault();
         // Wont submit the post if we are missing a room, fixture, or #of fixtures
         if (!roomInput.val().trim() || !fixtureInput.val().trim() || !qtyInput.val()) {
             alert("please fill out all fields");
-          return;
+            return;
         }
- // Constructing a newLine object to hand to the database
-    var newRoomLine = {
-      ProjectProjectId: {{project_id}},
-      PrefixturePreFixID: roomInput.val().trim(),
-      floor: {{floor_number}},
-      prefixture_id: fixtureInput.val().trim(),
-      quantity: qtyInput.val().trim()
-    };
+        // Constructing a newLine object to hand to the database
+        var newRoomLine = {
+            ProjectProjectId: {
+                { project_id } },
+            PrefixturePreFixID: roomInput.val().trim(),
+            floor: {
+                { floor_number } },
+            prefixture_id: fixtureInput.val().trim(),
+            quantity: qtyInput.val().trim()
+        };
 
-    console.log(newRoomLine.room);
+        console.log(newRoomLine.room);
+    }
 
+    $("#nextFloor").onClick(function{
+        queryurl= "/api/projects/"+
+         $.get("/api/projects/", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
 
-
-    // var row = $("<tr>");
-    // var td = $("<td>" + newRoomLine.room + "</td>");
-    //             row.append(td);
-    // var td = $("<td>" + newRoomLine.fixture + "</td>");
-    //             row.append(td);
-    // var td = $("<td>" + newRoomLine.qty + "</td>");
-    //  row.append(td);
-    // var td = $("<button>X</button>");
-    //             row.append(td);
-
-    // $("tbody").prepend(row);
-
-    
-
-
- }
+    })
 
 });
-
